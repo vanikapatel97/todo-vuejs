abecd771a2d63a7b63f62ad8333fa76fa5616520
Vanika Patel <vanikadpatel@gmail.com>
2021-11-07 17:36:38 +0000

    created file component 'ToDoForm'

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <h1>To-Do List</h1>
+    <to-do-form></to-do-form>
     <ul>
       <li v-for="item in ToDoItems" :key="item.id">
         <to-do-item :label="item.label" :done="item.done"></to-do-item>
@@ -12,11 +13,13 @@
 <script>
 import ToDoItem from './components/ToDoItem.vue';
 import uniqueId from 'lodash.uniqueid';
+import ToDoForm from './components/ToDoForm';
 
 export default {
   name: 'app',
   components: {
-    ToDoItem
+    ToDoItem,
+    ToDoForm
   },
   data(){
     return {
